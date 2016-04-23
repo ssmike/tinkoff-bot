0f6d91efbdfb5b3cffd4d7bde5fb478ce2ce4fef
Софиенко Александр Анатольевич <florinok@yandex.ru>
2016-04-23 18:09:40 +0300

    many questions

--- parse.py
+++ parse.py
@@ -7,17 +7,27 @@ with open("FAQ.txt", "r") as f:
 current_index = 0
 idx = 1
 parsed = []
+stop = "++++"
 ES_HOST = os.getenv('ES_HOST', default='localhost')
 es = Elasticsearch([ES_HOST])
 while current_index < len(data):
-    question = data[current_index]
+    question = []
+    i = 0
+    while (current_index + i < len(data) and data[current_index + i][:-1] != stop):
+        question.append( data[current_index+i][:-1])
+        i += 1
+
+    current_index += i
     i = 0
     while (current_index + i < len(data) and data[current_index + i] != '\n'):
         i += 1
     answer = "".join(data[current_index + 1:current_index + i])
-    print ("ID: " + str(idx))
-    print("Q: " + question)
-    print("A: " + answer)
-    current_index += (i + 1)
-    idx += 1
-    es.index("faqs", "faq", {"question": question, "answer": answer}, idx)
+
+    for n,q in enumerate(question):
+        es.index("faqs", "faq", {"question": q, "answer": answer}, idx+n)
+#        print ("ID: " + str(idx+n))
+#        print("Q: " + q)
+#        print("A: " + answer)
+
+    current_index += i+1
+    idx += len(question)
