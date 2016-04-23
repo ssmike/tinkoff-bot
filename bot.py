5ea1849598afcc72a09eb444212025485df54086
penguin138 <fridaythethirteens@gmail.com>
2016-04-23 16:30:00 +0300

    fix pep8 and add response for non-text messages

--- bot.py
+++ bot.py
@@ -10,16 +10,22 @@ bot = telepot.Bot(TELEGRAM_API_KEY)
 
 
 def handle(msg):
-    q = msg['text']
+    if 'text' in msg:
+        q = msg['text']
+    else:
+        bot.sendMessage(msg['chat']['id'],
+                        'Извините, я могу отвечать только на текстовые сообщения.')
     res = es.search(body={"query": {"query_string": {"query": q}}})
     # print(res)
     if (len(res['hits']['hits']) == 0 or res['hits']['hits'][0]['_score'] < 0.3):
-        bot.sendMessage(msg['chat']['id'], 'В данный момент я не могу ответить на ваш вопрос. Попробуйте позднее.')
+        bot.sendMessage(msg['chat']['id'],
+                        'В данный момент я не могу ответить на ваш вопрос. Попробуйте позднее.')
     else:
         ques = res['hits']['hits'][0]['_source']['question']
         ans = res['hits']['hits'][0]['_source']['answer']
         score = res['hits']['hits'][0]['_score']
-        formatted = "Relevancy:{rel}\nВопрос: {question}\nОтвет: {answer}".format(rel=score, question=ques, answer=ans)
+        format_string = "Relevancy:{rel}\nВопрос: {question}\nОтвет: {answer}"
+        formatted = format_string.format(rel=score, question=ques, answer=ans)
         bot.sendMessage(msg['chat']['id'], formatted)
 
 print(bot.getMe())
