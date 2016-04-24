766bca8f400ab6a909dcf585b1e248530eaffb1c
Andrew Stepanov <karn9050@gmail.com>
2016-04-24 10:27:53 +0300

    Improve manage.py and bot.py

--- parse.sh
+++
@@ -1,5 +0,0 @@
-sleep 5
-curl -XDELETE http://$ES_HOST:9200/faqs
-curl -XPUT http://$ES_HOST:9200/faqs
-curl -XPOST http://$ES_HOST:9200/faqs/faq/_mapping -d'{"faq": {"_all": {"analyzer": "russian_morphology"}}}'
-python parse.py $1
