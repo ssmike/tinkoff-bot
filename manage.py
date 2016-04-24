95bb5e0f996aed770fea352a99ff2adb213f986a
Andrew Stepanov <karn9050@gmail.com>
2016-04-24 11:06:42 +0300

    Fix manage.py

--- manage.py
+++ manage.py
@@ -5,21 +5,30 @@ import time
 
 ES_HOST = os.getenv('ES_HOST', default='localhost')
 es = Elasticsearch([ES_HOST])
-while not es.ping():
-    print("Waiting for elasticsearch to launch...")
+
+good = False
+while not good:
+    good = False
     time.sleep(1)
+    try:
+        while not es.ping():
+            print("Waiting for elasticsearch to launch...")
+            time.sleep(1)
+        good = True
+    except:
+        good = False
 
 command = sys.argv[1]
 index = sys.argv[2]
 if (command == "delete"):
     os.system("curl -XDELETE http://$ES_HOST:9200/" + index)
-if (command == "create"):
+elif (command == "create"):
     os.system("curl -XPUT http://$ES_HOST:9200/" + index)
     os.system("""curl -XPOST http://$ES_HOST:9200/""" +
               index + """/""" + index +
               """/_mapping -d'{\"""" + index +
               """\":{"_all":{"analyzer":"russian_morphology"}}}'""")
-if (command == "add"):
+elif (command == "add"):
     filename = sys.argv[3]
     with open(filename, "r") as f:
         data = f.readlines()
